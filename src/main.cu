2f34eaac575e6813ec90ee8c9758995dd455393e
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2020-11-04 18:30:22 +0100

    Remove multi GPU support, yeet threading, add --region

--- src/main.cu
+++ src/main.cu
@@ -1,16 +1,11 @@
 #include <algorithm>
-#include <atomic>
 #include <chrono>
 #include <climits>
 #include <cmath>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
-#include <mutex>
-#include <set>
 #include <string>
-#include <thread>
-#include <vector>
 
 #include "boinc_api.h"
 
@@ -31,8 +26,6 @@
 #define LOG_FILE "clusters.txt"
 #define STATE_FILE ".state"
 
-#define EXTENTS 512
-
 #define STACK_SIZE 128
 
 // 32x32 cache around the examined chunk.
@@ -44,13 +37,14 @@
 
 #define UINT64_BITS (sizeof(uint64_t) * 8)
 
-const size_t CHUNKS_PER_SEED = (EXTENTS * 2UI64) * (EXTENTS * 2UI64);
 const size_t CACHE_SIZE_BITS = (CACHE_EXTENTS * 2UI64) * (CACHE_EXTENTS * 2UI64);
 const size_t CACHE_SIZE_UINT64 = ((CACHE_SIZE_BITS + (UINT64_BITS - 1)) / UINT64_BITS);
 
 // Device code:
 
 __constant__ uint8_t c_min_cluster_size;
+__constant__ int32_t c_search_region_extents;
+__constant__ uint64_t c_chunks_per_seed;
 
 class Offset {
 public:
@@ -104,13 +98,13 @@ DEVICE_CALL Cluster explore_cluster(BitField &cache, Stack<Offset> &stack, JavaR
 
         cluster_size++;
 
-        if(chunk_x + 1 < EXTENTS && offset.x + 1 < CACHE_EXTENTS)
+        if(chunk_x + 1 < c_search_region_extents && offset.x + 1 < CACHE_EXTENTS)
             stack.push(Offset(offset.x + 1, offset.z));
-        if(chunk_x - 1 >= -EXTENTS && offset.x - 1 >= -CACHE_EXTENTS)
+        if(chunk_x - 1 >= -c_search_region_extents && offset.x - 1 >= -CACHE_EXTENTS)
             stack.push(Offset(offset.x - 1, offset.z));
-        if(chunk_z + 1 < EXTENTS && offset.z + 1 < CACHE_EXTENTS)
+        if(chunk_z + 1 < c_search_region_extents && offset.z + 1 < CACHE_EXTENTS)
             stack.push(Offset(offset.x, offset.z + 1));
-        if(chunk_z - 1 >= -EXTENTS && offset.z - 1 >= -CACHE_EXTENTS)
+        if(chunk_z - 1 >= -c_search_region_extents && offset.z - 1 >= -CACHE_EXTENTS)
             stack.push(Offset(offset.x, offset.z - 1));
     }
 
@@ -120,11 +114,14 @@ DEVICE_CALL Cluster explore_cluster(BitField &cache, Stack<Offset> &stack, JavaR
 __global__ void kernel(uint64_t block_count, uint64_t offset, uint64_t *collector_size, Cluster *collector) {
     uint64_t world_seed = blockIdx.x + offset;
 
-    uint64_t min_chunks_per_thread = CHUNKS_PER_SEED / blockDim.x; // Preferred number of chunks to be processed by a thread.
+    // Preferred number of chunks to be processed by a single thread:
+    uint64_t min_chunks_per_thread = c_chunks_per_seed / blockDim.x;
     uint64_t starting_chunk = threadIdx.x * min_chunks_per_thread;
     uint64_t chunks_here; // Number of chunks processed by this thread.
-    if(threadIdx.x == blockDim.x - 1) // The last thread will process any extra chunks (shouldn't happen with a good BLOCK_SIZE).
-        chunks_here = CHUNKS_PER_SEED - (blockDim.x - 1) * min_chunks_per_thread;
+
+    // The last thread will process any extra chunks (shouldn't happen with a good BLOCK_SIZE).
+    if(threadIdx.x == blockDim.x - 1) 
+        chunks_here = c_chunks_per_seed - (blockDim.x - 1) * min_chunks_per_thread;
     else
         chunks_here = min_chunks_per_thread;
 
@@ -138,8 +135,8 @@ __global__ void kernel(uint64_t block_count, uint64_t offset, uint64_t *collecto
     // We iterate in a specific manner to slightly improve caching performance:
     for(uint64_t i = 0; i < chunks_here; i++) {
         uint64_t chunk_idx = i + starting_chunk;
-        int32_t chunk_x = chunk_idx / (EXTENTS * 2) - EXTENTS;
-        int32_t chunk_z = chunk_idx % (EXTENTS * 2) - EXTENTS;
+        int32_t chunk_x = (chunk_idx / (c_search_region_extents * 2)) - c_search_region_extents;
+        int32_t chunk_z = (chunk_idx % (c_search_region_extents * 2)) - c_search_region_extents;
 
         cache.clear();
         // Stack is left empty after very call to explore_cluster(...), so there's no need to clear it here.
@@ -153,37 +150,82 @@ __global__ void kernel(uint64_t block_count, uint64_t offset, uint64_t *collecto
 
 // End of device code.
 
-FILE *log_file, *state_file;
-uint64_t offset = 0;
-std::atomic<uint64_t> found_total(0);
-std::atomic<int32_t> num_devices_running(0);
-std::mutex stderr_mutex, log_file_mutex, offset_mutex;
-
-void log(const std::string &text) {
-    std::lock_guard<std::mutex> log_file_guard(log_file_mutex);
-    fprintf(log_file, "%s\n", text.c_str());
-    fflush(log_file);
+uint64_t get_millis() {
+	auto epoch = std::chrono::system_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(epoch).count();
 }
 
-void info(int32_t device_index, const std::string &text) {
-    std::lock_guard<std::mutex> stderr_guard(stderr_mutex);
-    if(device_index < 0)
-        fprintf(stderr, "[HOST/INFO]: %s\n", text.c_str());
+//
+// Accepted options:
+// -s <int> | --start   | what seed to start the search at (inclusive) (default: 0)
+// -e <int> | --end     | what seed to end the search at (inclusive) (default: 0)
+// -c <int> | --cluster | set the minimum searched size of a cluster (default: 25)
+// -r <int> | --region  | resize the search region, this value is half the side of the bounding rectangle (default: 512)
+// 
+int32_t main(int32_t argc, char **argv) {
+    uint64_t start_offset = 0, end_offset = 0, offset = 0;
+    uint8_t min_cluster_size = 25;
+    int32_t search_region_extents = 512;
+    
+    for(int32_t i = 1; i < argc; i += 2) {
+        const char *option = argv[i], *value = argv[i + 1];
+
+        if(strcmp(option, "-s") == 0 || strcmp(option, "--start") == 0)
+            sscanf(value, "%I64u", &start_offset);
+        else if(strcmp(option, "-e") == 0 || strcmp(option, "--end") == 0)
+            sscanf(value, "%I64u", &end_offset);
+        else if(strcmp(option, "-c") == 0 || strcmp(option, "--cluster") == 0)
+            sscanf(value, "%I8u", &min_cluster_size);
+        else if(strcmp(option, "-r") == 0 || strcmp(option, "--region") == 0)
+            sscanf(value, "%I32", &search_region_extents);
+    }
+    if(end_offset < start_offset)
+        end_offset = start_offset;
+
+    BOINC_OPTIONS options;
+
+    boinc_options_defaults(options);
+    options.normal_thread_priority = true; // Since we're using CUDA.
+    boinc_init_options(&options);
+
+    APP_INIT_DATA aid;
+    boinc_get_init_data(aid);
+    
+    int32_t device_index = 0;
+    if(aid.gpu_device_num > 0)
+        fprintf(stderr, "Setting up BOINC specified device #%I32\n", device_index = aid.gpu_device_num);
     else
-        fprintf(stderr, "[DEVICE #%d/INFO]: %s\n", device_index,  text.c_str());
-    fflush(stderr);
-}
+        fprintf(stderr, "Using  default device #0.\n");
 
-void error(int32_t device_index, const std::string &text) {
-    std::lock_guard<std::mutex> stderr_guard(stderr_mutex);
-    if(device_index < 0)
-        fprintf(stderr, "[HOST/ERROR]: %s\n", text.c_str());
-    else
-        fprintf(stderr, "[DEVICE #%d/ERROR]: %s\n", device_index,  text.c_str());
-    fflush(stderr);
-}
+    std::string boinc_log_path, boinc_state_path;
+    if(boinc_resolve_filename_s(LOG_FILE, boinc_log_path))
+        boinc_temporary_exit(5, "Failed to access log file.", true);
+    if(boinc_resolve_filename_s(STATE_FILE, boinc_state_path))
+        boinc_temporary_exit(5, "Failed to access checkpoint file.", true);
 
-void manage_device(int32_t device_index, uint64_t start, uint64_t end, uint8_t min_cluster_size) {
+    FILE *log_file = boinc_fopen(boinc_log_path.c_str(), "a");
+
+    FILE *state_file = boinc_fopen(boinc_state_path.c_str(), "r");
+    if(state_file == nullptr) {
+        fprintf(stderr, "No checkpoint yet available.\n");
+        offset = start_offset;
+    } else {
+        fscanf(state_file, "%I64u", &offset);
+        fclose(state_file);
+        if(offset < start_offset || offset > end_offset) {
+            fprintf(stderr, "Checkpoint was outdated.\n");
+            offset = start_offset;
+        } else
+            fprintf(stderr, "Loaded checkpoint: %I64u\n", offset);
+    }
+
+    fprintf(stderr, "Start offset: %I64u\n", start_offset);
+    fprintf(stderr, "End offset: %I64u\n", end_offset);
+    fprintf(stderr, "Current offset: %I64u\n", offset);
+    fprintf(stderr, "Required cluster size: %I8u\n", min_cluster_size);
+
+    // Init CUDA device:
+
     cudaSetDevice(device_index);
 
     // Approximate the number of threads that has to be launched per wave for maximum performance:
@@ -192,9 +234,13 @@ void manage_device(int32_t device_index, uint64_t start, uint64_t end, uint8_t m
     int32_t pref_wave_size = prop.maxThreadsPerMultiProcessor * prop.multiProcessorCount;
     int32_t max_block_count = pref_wave_size / BLOCK_SIZE;
 
-    info(device_index, "Blocks per wave: " + std::to_string(max_block_count));
+    fprintf(stderr, "Blocks per wave: %I32\n", max_block_count);
 
+    // Allocate constants:
+    uint64_t chunks_per_seed = (search_region_extents * 2UI64) * (search_region_extents * 2UI64);
     cudaMemcpyToSymbol(c_min_cluster_size, &min_cluster_size, sizeof(uint8_t));
+    cudaMemcpyToSymbol(c_search_region_extents, &search_region_extents, sizeof(int32_t));
+    cudaMemcpyToSymbol(c_chunks_per_seed, &chunks_per_seed, sizeof(uint64_t));
 
     // Found items are collected in video memory.
     // Any collected items will be transferred to the host in-between work unit executions.
@@ -202,20 +248,20 @@ void manage_device(int32_t device_index, uint64_t start, uint64_t end, uint8_t m
     Cluster *d_collector;
     cudaMalloc(&d_collector_size, sizeof(uint64_t));
     cudaMalloc(&d_collector, MAX_COLLECTOR_SIZE * sizeof(Cluster));
-    
-    // Offset is also accessed from within the while condition.
-    offset_mutex.lock();
-    while(offset <= end) {
+
+    uint64_t total_found = 0;
+    int32_t secs_since_checkpoint = 0;
+    uint64_t timer = get_millis();
+    while(offset <= end_offset) {
         // Reset caches:
         cudaMemset(d_collector_size, 0, sizeof(uint64_t));
 
         // Launch one wave (one block does one seed):
-        uint64_t block_count = end - offset + 1; // +1, cause {end} is inclusive
+        uint64_t block_count = end_offset - offset + 1; // +1, cause {end} is inclusive
         if(block_count > max_block_count)
             block_count = max_block_count;
         kernel<<<block_count, BLOCK_SIZE>>>(block_count, offset, d_collector_size, d_collector);
         offset += block_count;
-        offset_mutex.unlock();
 
         // Synchronize:
         cudaDeviceSynchronize();
@@ -223,8 +269,7 @@ void manage_device(int32_t device_index, uint64_t start, uint64_t end, uint8_t m
         // Catch errors:
         cudaError_t code;
         if((code = cudaGetLastError()) != cudaSuccess) {
-            error(device_index, cudaGetErrorString(code));
-            offset_mutex.lock();
+            fprintf(stderr, "CUDA ERROR: %s\n", cudaGetErrorString(code));
             break;
         }
 
@@ -232,125 +277,31 @@ void manage_device(int32_t device_index, uint64_t start, uint64_t end, uint8_t m
         uint64_t collector_size;
         cudaMemcpy(&collector_size, d_collector_size, sizeof(uint64_t), cudaMemcpyDeviceToHost);
 
-        std::vector<Cluster> clusters(collector_size);
-        cudaMemcpy(clusters.data(), d_collector, collector_size * sizeof(Cluster), cudaMemcpyDeviceToHost);
+        Cluster *clusters = new Cluster[collector_size];
+        cudaMemcpy(clusters, d_collector, collector_size * sizeof(Cluster), cudaMemcpyDeviceToHost);
 
-        auto end = clusters.end();
-        for(auto cluster = clusters.begin(); cluster != end; cluster++) {
+        Cluster *end = clusters + collector_size;
+        for(Cluster *cluster = clusters; cluster != end; cluster++) {
             // Remove all other duplicates of this cluster:
             end = std::remove(cluster + 1, end, *cluster);
 
             std::string cluster_info = cluster->to_string();
-            log(cluster_info);
-            info(device_index, cluster_info);
+            fprintf(log_file, "%s\n", cluster_info.c_str());
+            fflush(log_file);
 
-            found_total++;
+            total_found++;
         }
+        delete[] clusters;
 
-        offset_mutex.lock();
-    }
-    offset_mutex.unlock();
+        // Monitoring and checkpoints (executes every second):
 
-    cudaFree(d_collector);
-    cudaFree(d_collector_size);
-
-    num_devices_running--;
-}
-
-//
-// Accepted options:
-// -d <int> | --device  | specify a compute device to be used, use one time for each requested device
-// -s <int> | --start   | what seed to start the search at (inclusive) (default: 0)
-// -e <int> | --end     | what seed to end the search at (inclusive) (default: 0)
-// -c <int> | --cluster | set the minimum searched size of a cluster (default: 25)
-// 
-int32_t main(int32_t argc, char **argv) {
-    uint64_t start = 0, end = 0;
-    uint8_t min_cluster_size = 25;
-
-    // Use set to eliminate duplicate entries:
-    std::set<int32_t> requested_devices;
-    for(int32_t i = 1; i < argc; i += 2) {
-        const char *option = argv[i];
-        const char *value = argv[i + 1];
-
-        if(strcmp(option, "-d") == 0 || strcmp(option, "--device") == 0)
-            requested_devices.insert(atoi(value));
-        else if(strcmp(option, "-s") == 0 || strcmp(option, "--start") == 0)
-            sscanf(value, "%I64u", &start);
-        else if(strcmp(option, "-e") == 0 || strcmp(option, "--end") == 0)
-            sscanf(value, "%I64u", &end);
-        else if(strcmp(option, "-c") == 0 || strcmp(option, "--cluster") == 0)
-            sscanf(value, "%I8u", &min_cluster_size);
-    }
-    if(end < start)
-        end = start;
-
-    BOINC_OPTIONS options;
-
-    boinc_options_defaults(options);
-    options.multi_thread = true;
-    options.multi_process = true;
-    options.normal_thread_priority = true;
-    boinc_init_options(&options);
-
-    std::string boinc_log_path, boinc_state_path;
-    if(boinc_resolve_filename_s(LOG_FILE, boinc_log_path))
-        boinc_temporary_exit(5, "Failed to access log file.", true);
-    if(boinc_resolve_filename_s(STATE_FILE, boinc_state_path))
-        boinc_temporary_exit(5, "Failed to access checkpoint file.", true);
-
-    log_file = boinc_fopen(boinc_log_path.c_str(), "a");
-
-    state_file = boinc_fopen(boinc_state_path.c_str(), "r");
-    if(state_file == nullptr) {
-        info(-1, "No checkpoint yet available.");
-        offset = start;
-    } else {
-        fscanf(state_file, "%I64u", &offset);
-        fclose(state_file);
-        if(offset < start || offset > end) {
-            error(-1, "Checkpoint was outdated.");
-            offset = start;
-        } else
-            info(-1, "Loaded checkpoint: " + std::to_string(offset));
-    }
-
-    info(-1, "Start offset: " + std::to_string(start));
-    info(-1, "End offset: " + std::to_string(end));
-    info(-1, "Current offset: " + std::to_string(offset));
-    info(-1, "Required cluster size: " + std::to_string(min_cluster_size));
-
-    // Launch host threads:
-    int32_t num_devices_initialized = requested_devices.size();
-    num_devices_running.store(num_devices_initialized);
-    std::vector<std::thread> threads(num_devices_initialized);
-    for(int32_t device_index : requested_devices)
-        threads.push_back(std::thread(manage_device, device_index, start, end, min_cluster_size));
-
-    // Monitor progress:
-    int32_t secs_since_checkpoint = 0;
-    while(true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-        if(num_devices_running.load() < num_devices_initialized) {
-            std::lock_guard<std::mutex> offset_guard(offset_mutex);
-            if(offset <= end)
-                error(-1, "A compute device has encountered an unexpected shutdown. See further up for device logs.");
-            num_devices_initialized--;
-        }
-        if(num_devices_initialized < 1)
-            break;
-
-        uint64_t offset_snapshot;
-        {
-            std::lock_guard<std::mutex> offset_guard(offset_mutex);
-            offset_snapshot = offset;
-        }
+        if(get_millis() - timer < 1000)
+            continue;
+        timer = get_millis();
         
-        boinc_fraction_done((double)(offset_snapshot - start) / (end - start + 1));
+        boinc_fraction_done((double)(offset - start_offset) / (end_offset - start_offset + 1));
 
-        // 10 secs have passed or BOINC was suspended:
+        // 10 secs have passed or BOINC has been suspended:
         if(boinc_time_to_checkpoint() || ++secs_since_checkpoint >= 10) {
             boinc_begin_critical_section();
 
@@ -360,32 +311,28 @@ int32_t main(int32_t argc, char **argv) {
 
             // Actually, this should always execute:
             if(state_file != nullptr) {
-                fprintf(state_file, "%I64u", offset_snapshot);    
+                fprintf(state_file, "%I64u", offset);    
                 fclose(state_file);
             }
 
             boinc_checkpoint_completed();
             boinc_end_critical_section();
 
-            info(-1, "Checkpoint saved: " + std::to_string(offset_snapshot));
+            fprintf(stderr, "Checkpoint saved: %I64u\n", offset);
             secs_since_checkpoint = 0;
         }
     }
 
-    // Join host threads:
-    for(std::thread &thread : threads)
-        if(thread.joinable())
-            thread.join();
+    cudaFree(d_collector);
+    cudaFree(d_collector_size);
 
     fclose(log_file);
 
-    if(offset <= end)
-        return boinc_finish_message(1, "The program has finished but the task is not yet complete.\n"
-                "This indicates that a compute device has shut down during the process.\n"
-                "See the error logs for more information.", true);
-
-    uint64_t found_snapshot = found_total.load();
-    info(-1, "Finished, " + std::to_string(found_snapshot) + " cluster" + (found_snapshot == 1 ? " was" : "s were") + " found.");
+    if(offset <= end_offset)
+        return boinc_finish_message(1, "Program has finished but the task is not yet complete.\n"
+                "This indicates that your CUDA device has shut down during the process.\n"
+                "See error logs for more information.", true);
     
-    return boinc_finish(0);
+    return boinc_finish_message(0, ("Finished, " + std::to_string(total_found) + " cluster" +
+            (total_found == 1 ? " was" : "s were") + " found.").c_str(), false);
 }
